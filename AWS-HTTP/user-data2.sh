405f6a7170aaae3a84f12a40324edfbe5fad1fcc
yjunho0325 <wnsgh011400@gamil.com>
2026-07-16 12:59:57 +0900

    vs1_index.html 과제 업로드

--- AWS-HTTP/user-data2.sh
+++ AWS-HTTP/user-data2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt update -y
 apt install -y nginx
+curl -o /var/www/html/index.html https://raw.githubusercontent.com/yjunho0325/AWS/refs/heads/main/index2.html
 systemctl enable nginx
 systemctl restart nginx
--- user-data2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt update -y
-apt install -y nginx
-systemctl enable nginx
-systemctl restart nginx
